d5572651f7c3257257d9161358d9b7f7b3c3828c
Chengzhong Luo <luoc@usc.edu>
2024-04-22 18:53:30 -0700

    Add files via upload

--- phmeter.c
+++ phmeter.c
@@ -4,32 +4,13 @@
 
 #include "lcd459.h"
 #include "adcph.h"
-//ADC2 pin 25 PC2
-  //Store the average value of the sensor feedback
 
-void read_ph(void);
-
-int main(){
-
-    adc_init_ph();
-    lcd_init();
-    sci_init();
-
-    DDRC &= ~(1 << 1);
-    PORTC |= (1 << 1);
-
-    while(1){
-        sci_out(0xfe);              // Clear the screen
-        sci_out(0x51);
-        read_ph();
-        _delay_ms(2000);
-    }
-    return 0;
-}
+unsigned int phValue;
+unsigned int decimalVal;
 
 void read_ph(void){
     char bufph[20];
-    char bufraw[20];
+    //char bufraw[20];
     uint32_t avgValuetemp;
     uint16_t buf[10], temp;
 
@@ -61,16 +42,18 @@ void read_ph(void){
     uint16_t avgValue = avgValuetemp / 6; //avg adc value
     unsigned int milliv = avgValue * 5 / 10.24; //convert the analog into millivolt using 10.24 instead of 1024 to preserve decimal
     unsigned int tempph = 23 + (3.5 * milliv); // holding value = ph * 100 (+adjustment)
-    unsigned int phValue = tempph / 100; //getting truncated ph value
+    phValue = tempph / 100; //getting truncated ph value
 
-    unsigned int decimalVal = tempph % 100;
+    decimalVal = tempph % 100;
 
-    sprintf(bufph, "Raw: %u", avgValue);
+    /*
+    sprintf(bufraw, "Raw: %u", avgValue);
     lcd_moveto(0,0);
     lcd_stringout(bufph);
+    */
 
-    sprintf(bufraw, "pH: %u.%02u", phValue, decimalVal);
-    lcd_moveto(1,0);
-    lcd_stringout(bufraw);
+    sprintf(bufph, "pH: %u.%02u", phValue, decimalVal);
+    lcd_moveto(3,0);
+    lcd_stringout(bufph);
 }
 
